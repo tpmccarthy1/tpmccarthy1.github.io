a8a06845680b2d7b7c1982307431d5e29f4444d4
Tom McCarthy <tpmccarthy1@gmail.com>
2016-05-02 18:07:50 -0400

    Monday

--- js/main.js
+++ js/main.js
@@ -1,40 +1,4 @@
-var sliders = [];
-
 $(document).ready(function(){
-
-	 //Instagram Feed
-
-
-	 
-	 var userFeed = new Instafeed({
-	 get: 'tagged',
-	 tagName: 'wolfpack',
-	 accessToken: '12027293.467ede5.9c0cd6283fd9406f879b71e3de7595f4',
-	 useHttp: true,
-	 template: '<li><img src="{{image}}" /><span id="location">{{location}}</span></li>',
-	 target: 'instafeed',
-	 resolution: 'standard_resolution',
-	 sortBy: 'most-liked',
-	 limit: 10,
-	 after: function(){
-        $('.slider').each(function() {
-        sliders.push(new Slider(this))
-        })
-	 }
-	 });
-
-
-	 userFeed.run();
-
-	 //Buttons for Instafeed
-
-	 $(".btn-next").on('click', function(e){
-		sliders[0].goToNext();
-	});
-
-	 $(".btn-prev").on('click', function(e){
-		sliders[0].goToPrev();
-	});
 
 	 //Scroll to page 
 
@@ -47,6 +11,11 @@ $(document).ready(function(){
 	$(".about").on('click', function(e){
 		e.preventDefault();
 		$.scrollTo( '#main-content', 800);
+	});
+
+	$(".work").on('click', function(e){
+		e.preventDefault();
+		$.scrollTo( '#work', 800);
 	});
 
 
